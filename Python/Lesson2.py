fdc88a9ed6f408d7565935186eb241b58959a08a
Amy Phanphongsa-Syxomphou <aphanpo@gmail.com>
2020-01-07 19:32:08 -0800

    lesson 2 on codecademy python

---
+++ Python/Lesson2.py
@@ -0,0 +1,35 @@
+#Strings and Console output
+
+#use backslash ( \ ) if there is an apostrophe in a word if using single quotes
+'There\'s an apostrophe in that word'
+"There's an apostrophe in that word"
+
+#Will print " Y " because its the 4th index
+fifth_letter = "MONTY"[4]
+print (fifth_letter)
+
+#String Methods
+##len() returns the number of characters in a string
+parrot = "Norwegian Blue"
+print (len(parrot))
+#returns 14
+##.lower() returns all characters lower case.. only works with strings
+print (parrot.lower())
+##.upper() returns ALL CAPS.. only works with strings
+print (parrot.upper())
+##str() turns non-strings into strings
+print (str(2))
+
+#%s is a placeholder for a variable and using % followed by the variable will replace %s in order
+string_1 = "Camelot"
+string_2 = "place"
+
+print ("Let's not go to %s. It's a silly %s." % (string_1, string_2))
+
+#when you run this code, the console will ask each question and then populate the sentence
+name = input("What is your name? ")
+quest = input("What is your quest? ")
+color = input("What is your favorite color? ")
+
+print ("Ah, so your name is %s, your quest is %s, " \
+"and your favorite color is %s." % (name, quest, color))
